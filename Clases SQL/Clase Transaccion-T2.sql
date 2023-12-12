8bb19df36f98e2f4a6e3be1f079d4b3d05ccf7c2
nicovf11 <nicoveliz11@hotmail.com>
2023-12-12 02:20:07 -0300

    Create Clase Transaccion-T2.sql

---
+++ Clases SQL/Clase Transaccion-T2.sql
@@ -0,0 +1,79 @@
+USE UnlamBD
+
+-- SIN BLOQUEO
+
+BEGIN TRANSACTION
+
+SELECT *
+FROM Departamento
+
+UPDATE Departamento
+SET descripcion = 'Finanzas'
+WHERE descripcion = 'Administracion'
+
+COMMIT
+
+-- CON BLOQUEO
+
+-- T1 lee la tabla Empleado, luego modifica el salario de todos los empleados (les aumenta un 20%) y luego T2 intenta leer la tabla Empleado y se queda esperando a que T1 haga commit o rollback.
+
+BEGIN TRANSACTION
+
+SELECT *
+FROM Empleado
+
+COMMIT
+
+-- BLOQUEO A NIVEL FILA
+/* Caso 3: T1 bloquea una fila de la tabla Empleado y T2 puede modificar otra fila
+
+Cuando se coloca una condicion por la Primary Key de la tabla, entonces el bloqueo es por fila, y de esa forma permite que dos transacciones puedan hacer cambios en la misma tabla en forma simultanea, en distintas filas. Pero es impresindible que se coloque en el WHERE una condicion sobre la clave de la tabla.
+*/
+
+BEGIN TRANSACTION 
+
+
+UPDATE Empleado
+SET salario = salario * 3
+WHERE legajo = 105
+
+COMMIT
+
+
+/*Caso 4: T1 bloquea un subconjunto de filas y T2 no puede modificar otras filas
+
+En este caso, T1 modifica el salario de los Empleados de categoria A y luego T2 quiere modificar el salario de los empleados de categoria B, pero no puede hacerlo porque la tabla está bloqueada.
+
+Solo permite que dos transacciones hagan cambios en distintas filas de la misma tabla cuando se indica una condicion sobre la Clave Primaria de la tabla.
+*/
+
+BEGIN TRANSACTION
+
+UPDATE Empleado
+SET Salario = salario - 20000
+WHERE categoria = 'B'
+
+COMMIT
+
+
+-- SE BLOQUEAN LAS 2, Y MATA 1 ALEATORIAMENTE
+/*Caso 5: Deadlock
+
+T1 modifica la tabla Empleado y T2 modifica la tabla Departamento. Luego, T1 quiere leer la tabla Departamento pero no puede porque está bloqueada por T2 entonces se queda esperando y T2 quiere leer la tabla Empleado y tambien se queda esperando porque está bloqueada. Ambas transacciones se quedan esperando mutuamente, en forma infinita.
+
+Esto se llama Deadlock o Abrazo mortal. Cuando esto sucede, el motor de Base de Datos elije a una de las dos transacciones en forma aleatoria y la “mata” para que libere los recursos y la otra pueda continuar.
+*/
+
+
+BEGIN TRANSACTION
+
+SELECT *
+FROM Departamento
+
+INSERT INTO Departamento VALUES
+(6,'Contabilidad')
+
+SELECT *
+FROM Empleado
+
+COMMIT
